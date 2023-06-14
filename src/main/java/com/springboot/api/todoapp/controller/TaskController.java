7b624d85f1fd42fa73d8dcc94b54fff946ad6bae
Alawpak <alan281@hotmail.es>
2023-06-14 12:55:38 -0600

    se crea controlador para insertar tarea que puede usarse en swagger-ui.html

--- src/main/java/com/springboot/api/todoapp/controller/TaskController.java
+++ src/main/java/com/springboot/api/todoapp/controller/TaskController.java
@@ -1,8 +1,26 @@
 package com.springboot.api.todoapp.controller;
 
+import com.springboot.api.todoapp.persistence.entity.Task;
+import com.springboot.api.todoapp.service.TaskService;
+import com.springboot.api.todoapp.service.dto.TaskInDTO;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/tasks")
 public class TaskController {
 
+    private final TaskService taskService;
+
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @PostMapping
+    public Task createTask(@RequestBody TaskInDTO taskInDTO){
+        return this.taskService.createTask(taskInDTO);
+    }
 }
